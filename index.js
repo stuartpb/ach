44aa4d28c307037ff98ab61f6b6211ed72ca3c20
Stuart P. Bentley <stuart@testtrack4.com>
2014-01-17 03:00:12 -0500

    Allow constructor to be called without options

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = function ach(options) {
+  options = options || {};
   var params = {};
   var paramKeys = ['allowOrigin','allowHeaders','allowCredentials',
     'allowMethods','exposeHeaders','maxAge'];
