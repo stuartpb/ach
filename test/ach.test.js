bcddfbf035d75cb7b3a3e39817e59256ff44c7f1
Stuart P. Bentley <stuart@testtrack4.com>
2014-01-17 02:52:08 -0500

    Add test

---
+++ test/ach.test.js
@@ -0,0 +1,30 @@
+var request = require('supertest');
+var express = require('express');
+
+describe("CORS headers", function() {
+  describe("for ach defaults", function() {
+    var app = express();
+
+    app.get('/', function(req, res) {
+      res.send(200);
+    });
+    describe('origin-less requests', function() {
+      it('should not receive any Access-Control headers', function(done) {
+        request(app)
+          .get('/')
+          .expect(200)
+          .end(function(err, res) {
+            if (err) throw err;
+
+            Object.keys(res.header).forEach(function(header) {
+              if (/^access\-control/.test(header)) {
+                throw new Error (
+                  header + ' header present for originless request');
+              }
+            });
+            done();
+          });
+      });
+    });
+  });
+});
