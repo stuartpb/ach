242ac41b2b90d146d39e0509bb1b86d2043e4b97
Stuart P. Bentley <stuart@testtrack4.com>
2014-01-17 02:59:36 -0500

    Rename tests

--- test/ach.test.js
+++ test/ach.test.js
@@ -2,16 +2,17 @@ var request = require('supertest');
 var express = require('express');
 var ach = require('../index.js');
 
-describe("CORS headers", function() {
-  describe("for ach defaults", function() {
+describe("using ach", function() {
+  describe("with defaults", function() {
     var app = express();
 
     app.use(ach());
     app.get('/', function(req, res) {
       res.send(200);
     });
-    describe('origin-less requests', function() {
-      it('should not receive any Access-Control headers', function(done) {
+    
+    describe('requests with no Origin', function() {
+      it('should not receive Access-Control headers', function(done) {
         request(app)
           .get('/')
           .expect(200)
