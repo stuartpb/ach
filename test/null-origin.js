815127464812ca2f3be0347811374063c460886b
Stuart P. Bentley <stuart@testtrack4.com>
2014-01-17 03:46:25 -0500

    Add tests for null allowed origin list

---
+++ test/null-origin.js
@@ -0,0 +1,51 @@
+var request = require('supertest');
+var express = require('express');
+var ach = require('../index.js');
+
+describe("using ach", function() {
+  describe("with null allowOrigin", function() {
+    var app = express();
+
+    app.use(ach({allowOrigin: null}));
+    app.get('/', function(req, res) {
+      res.send(200);
+    });
+
+    describe('requests with no Origin', function() {
+      it('should not receive Access-Control headers', function(done) {
+        request(app)
+          .get('/')
+          .expect(200)
+          .end(function(err, res) {
+            if (err) throw err;
+
+            Object.keys(res.header).forEach(function(header) {
+              if (/^access\-control/.test(header)) {
+                throw new Error (
+                  header + ' header present for originless request');
+              }
+            });
+            done();
+          });
+      });
+    });
+
+    describe('requests with Origin set', function() {
+      it('should not receive Access-Control headers', function(done) {
+        request(app)
+          .get('/')
+          .expect(200)
+          .end(function(err, res) {
+            if (err) throw err;
+
+            Object.keys(res.header).forEach(function(header) {
+              if (/^access\-control/.test(header)) {
+                throw new Error (
+                  header + ' header present for originless list');
+              }
+            });
+            done();
+          });
+      });
+    });
+});
